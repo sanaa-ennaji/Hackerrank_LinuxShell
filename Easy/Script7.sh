f7b91467e48caaee0010a9576e7c4e2651a5adc1
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-18 10:37:28 +0100

    add script 7

---
+++ Easy/Script7.sh
@@ -0,0 +1,16 @@
+# Read in one character from STDIN.
+# If the character is 'Y' or 'y' display "YES".
+# If the character is 'N' or 'n' display "NO".
+# No other character will be provided as input.
+
+read -p "enter a character" char
+
+if [ "$char" = "Y" ] ||[ "$char" = "y" ]; then 
+
+   echo "YES"
+
+else
+
+    echo "NO"
+ 
+fi
